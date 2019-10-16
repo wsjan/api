e0523f2d426832fdb932e3fdd921ed3b8db58001
wsjan <wsjanisz@gmail.com>
2019-10-17 00:04:10 +0200

    smth

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ from typing import List
 from datetime import datetime
 import pandas as pd
 from sqlite3 import connect
-from api.wykres import create_plot
+from app.wykres import create_plot
 
 #%%
 def is_browser(request):
